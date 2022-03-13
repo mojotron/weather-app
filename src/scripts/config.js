43b5486f675ee163ee5d5e9302b46fc781481049
mojotron <igormalj@gmail.com>
2022-03-13 22:22:53 +0100

    Add logic to get weather data for current browser location - using geolocation API and openweathermap API

---
+++ src/scripts/config.js
@@ -0,0 +1,2 @@
+export const API_URL =
+  'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,3 +1,40 @@
 import '../styles/main.css';
 
-alert('yo');
+// import { API_URL } from './config';
+import { WEATHER_API_KEY } from './apikey';
+
+const state = {
+  forecast: {},
+  lat: null,
+  lng: null,
+};
+
+const getJSON = async url => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    state.forecast = {};
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const temp = data => {
+  console.log(data);
+  const { latitude, longitude } = data.coords;
+  state.lat = latitude;
+  state.lng = longitude;
+  console.log(state);
+  getJSON(
+    `http://api.openweathermap.org/data/2.5/weather?lat=${state.lat}&lon=${state.lng}&appid=${WEATHER_API_KEY}`
+  );
+};
+
+const getCurrentLocation = async () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(temp);
+  }
+};
+
+getCurrentLocation();
