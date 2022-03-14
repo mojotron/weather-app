4dbc3e2327c758d4761f02c519edf2ba487ecd11
mojotron <igormalj@gmail.com>
2022-03-14 21:18:11 +0100

    Refactor and promisify geolocation

--- src/scripts/config.js
+++ src/scripts/config.js
@@ -1,2 +1,5 @@
 export const API_URL =
   'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}';
+
+// search by city name with 5 match limit
+// http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=5&appid={API key}
--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -3,38 +3,47 @@ import '../styles/main.css';
 // import { API_URL } from './config';
 import { WEATHER_API_KEY } from './apikey';
 
-const state = {
-  forecast: {},
-  lat: null,
-  lng: null,
-};
+// const state = {
+//   forecast: {},
+//   lat: null,
+//   lon: null,
+// };
 
 const getJSON = async url => {
   try {
     const response = await fetch(url);
     const data = await response.json();
     console.log(data);
-    state.forecast = {};
   } catch (error) {
     console.error(error);
   }
 };
 
-const temp = data => {
-  console.log(data);
-  const { latitude, longitude } = data.coords;
-  state.lat = latitude;
-  state.lng = longitude;
-  console.log(state);
-  getJSON(
-    `http://api.openweathermap.org/data/2.5/weather?lat=${state.lat}&lon=${state.lng}&appid=${WEATHER_API_KEY}`
-  );
+const getCurrentLocation = () =>
+  // Promisify geolocation api
+  new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+
+const getCurrentLocationCoords = data => {
+  const { latitude: lat, longitude: lon } = data.coords;
+  return { lat, lon };
 };
 
-const getCurrentLocation = async () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(temp);
-  }
-};
+// get weather data with current geolocation
+getCurrentLocation()
+  .then(response => getCurrentLocationCoords(response))
+  .then(coords => {
+    getJSON(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${WEATHER_API_KEY}`
+    );
+  })
+  .catch(error => console.log(error.message));
 
-getCurrentLocation();
+// get weather data with city/country name
+// const city = 'rovinj';
+
+// search by city, get up to 5 search results
+// getJSON(
+//   `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${WEATHER_API_KEY}`
+// );
